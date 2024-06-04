ed56302f05fb1661ebe86e6a9d3faf9aaca6e344
eduardo.assuncao <eduardo.assuncao@tce.ma.gov.br>
2024-06-04 13:03:53 -0300

    mapped ManyToMany with association class

--- src/main/java/br/gov/ma/tce/dscommerce/entities/Product.java
+++ src/main/java/br/gov/ma/tce/dscommerce/entities/Product.java
@@ -26,6 +26,9 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {}
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
