be37857101f0a3b6b1d514f67aa8f069bc86f8c9
Eduardo (Kenny8855) Assunção <39742355+eduardoAssuncao@users.noreply.github.com>
2024-06-10 10:55:29 -0300

    Bean Validation

--- dscommerce/src/main/java/br/gov/ma/tce/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ dscommerce/src/main/java/br/gov/ma/tce/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package br.gov.ma.tce.dscommerce.controllers.handlers;
 
 import br.gov.ma.tce.dscommerce.dto.CustomError;
+import br.gov.ma.tce.dscommerce.dto.ValidationError;
 import br.gov.ma.tce.dscommerce.services.exceptions.DatabaseException;
 import br.gov.ma.tce.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,6 +29,17 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> dataBase (DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) //config para interceptar esse tipo de exception
+    public ResponseEntity<CustomError> methodArgumentNotValidation (MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        // for para percorrer todos os erros dentro das lista de exceção e lhe dando o apelodo de f
+        for (FieldError f : e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
--- dscommerce/src/main/java/br/gov/ma/tce/dscommerce/dto/ProductDTO.java
+++ dscommerce/src/main/java/br/gov/ma/tce/dscommerce/dto/ProductDTO.java
@@ -1,6 +1,9 @@
 package br.gov.ma.tce.dscommerce.dto;
 
 import br.gov.ma.tce.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class ProductDTO {
+    //As validações não estão funcionando -> EU SÓ TINHA QUE REINICIAR O PROJETO
     private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisar ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido") //é a junção do @NotNull e  @NotEmpty
     private String name;
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
