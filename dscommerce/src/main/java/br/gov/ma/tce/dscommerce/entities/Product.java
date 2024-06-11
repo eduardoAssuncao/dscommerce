6a868468be6f42f9409972dc7739dcb3e17ca9f4
Eduardo (Kenny8855) Assunção <39742355+eduardoAssuncao@users.noreply.github.com>
2024-06-11 10:15:06 -0300

    Add files via upload

--- dscommerce/src/main/java/br/gov/ma/tce/dscommerce/entities/Product.java
+++ dscommerce/src/main/java/br/gov/ma/tce/dscommerce/entities/Product.java
@@ -1,10 +1,8 @@
 package br.gov.ma.tce.dscommerce.entities;
 
-import br.gov.ma.tce.dscommerce.dto.ProductDTO;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.List;
